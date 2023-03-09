e31dd4a830b4c96cc5eaaf1cc8ac037efe3808cb
Peter <phellwege1@gmail.com>
2023-03-09 09:59:42 -0700

    added home page

---
+++ google_ml/src/views/home.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect, useRef} from 'react';
+import './main.css';
+import { Button, Card, Form, Alert} from 'react-bootstrap';
+
+
+export default (props) => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
